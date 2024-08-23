097b4bb97e8af28b37a1c9e8d8f6f7b4a31761ea
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-23 22:23:10 +0600

    add home and shared pages

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Navbar from "../../Shared/Navbar/Navbar";
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Navbar></Navbar>
+        </>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import Home from "../src/Pages/Home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
